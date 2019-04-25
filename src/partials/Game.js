06071a6ecd1c7ccb7327f256c6ba7a3fece74d92
Rishab <rishabagrawal@hotmail.com>
2019-04-25 15:27:41 -0700

    paddle for player 1 and player 2

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,7 +1,6 @@
-
-import{SVG_NS} from "../settings";
-
-// console.log(SVG_NS);
+import { SVG_NS, KEYS } from "../settings";
+import Paddle from "./Paddle";
+import Board from "./Board";
 
 export default class Game {
   constructor(element, width, height) {
@@ -9,22 +8,46 @@ export default class Game {
     this.width = width;
     this.height = height;
 
-    
     this.gameElement = document.getElementById(this.element);
 
-		// Other code goes here...
+    this.board = new Board(this.width, this.height);
+    
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+    
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2)
+    );
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      (this.width - this.boardGap - this.paddleWidth),
+      ((this.height - this.paddleHeight) / 2)
+    );
+    console.log(this.player1);
+    console.log(this.player2);
   }
-  
 
   render() {
-    let svg = document.createElementNS(SVG_NS, "svg");
-svg.setAttributeNS(null, "width", this.width);
-svg.setAttributeNS(null, "height", this.height);
-svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-this.gameElement.appendChild(svg);
+    // be sure to empty out the last frame before re-rendering
+    this.gameElement.innerHTML = "";
 
-this.gameElement.innerHTML = '';
-		// More code goes here....
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+
+
+    this.gameElement.appendChild(svg);
+
+    this.board.render(svg);
+    this.player1.render(svg);
+    this.player2.render(svg);
   }
-  
 }
